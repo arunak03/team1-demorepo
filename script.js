505120023682f6d1e98747ec7aaa09b12c9b305c
Aruna.K <arunak03022001@gmail.com>
2023-10-20 17:54:54 +0530

    Aruna Commit

--- script.js
+++ script.js
@@ -23,4 +23,21 @@ function clickHandler(){
         var displayedImage = document.getElementById('displayed-image');
         displayedImage.src = imageUrl
     }
+
+    function showSegment(segmentId,linkId){
+        const segments =document.querySelectorAll('.segment');
+        segments.forEach(segment=>{
+            segment.style.display='none';  
+        });
+       const selectedSegment = document.getElementById(segmentId);
+       selectedSegment.style.display='block';
+    
+       const links = document.querySelectorAll('.nav-link');
+       links.forEach(link =>{
+        link.classList.remove('active-link');
+       })
+       const selectedLink = document.getElementById(linkId);
+       selectedLink.classList.add('active-link');
+    }
+    
     
